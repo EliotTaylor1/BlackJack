025af3743d5093d870a63e25d27000d9b9299b28
Eliot Taylor <104195627+EliotTaylor1@users.noreply.github.com>
2023-06-02 18:10:56 +0100

    Add project files.

---
+++ Card.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    internal class Card
+    {
+        public enum Rank
+        {
+            Rank_2,
+            Rank_3,
+            Rank_4,
+            Rank_5,
+            Rank_6,
+            Rank_7,
+            Rank_8,
+            Rank_9,
+            Rank_10,
+            Rank_Jack,
+            Rank_Queen,
+            Rank_King,
+            Rank_Ace
+        }
+        public enum Suit
+        {
+            Suit_Club,
+            Suit_Spade,
+            Suit_Heart,
+            Suit_Diamond
+        }
+        public Card(Rank rank, Suit suit)
+        {
+            _rank = rank;
+            _suit = suit;
+         }
+        public Rank _rank;
+        public Suit _suit;
+
+        public string PrintCard()
+        {
+            string cardRank = _rank switch
+            {
+                Rank.Rank_2 => "2",
+                Rank.Rank_3 => "3",
+                Rank.Rank_4 => "4",
+                Rank.Rank_5 => "5",
+                Rank.Rank_6 => "6",
+                Rank.Rank_7 => "7",
+                Rank.Rank_8 => "8",
+                Rank.Rank_9 => "9",
+                Rank.Rank_10 => "10",
+                Rank.Rank_Jack => "Jack",
+                Rank.Rank_Queen => "Queen",
+                Rank.Rank_King => "King",
+                Rank.Rank_Ace => "Ace",
+                _ => "Unknown",
+            };
+            string cardSuit = _suit switch
+            {
+                Suit.Suit_Club => "Club",
+                Suit.Suit_Spade => "Spade",
+                Suit.Suit_Heart => "Heart",
+                Suit.Suit_Diamond => "Diamond",
+                _ => "Unknown",
+            };
+            string card = cardRank + cardSuit;
+            return card;
+        }
+        public int GetCardValue()
+        {
+            return _rank switch
+            {
+                Rank.Rank_2 => 2,
+                Rank.Rank_3 => 3,
+                Rank.Rank_4 => 4,
+                Rank.Rank_5 => 5,
+                Rank.Rank_6 => 6,
+                Rank.Rank_7 => 7,
+                Rank.Rank_8 => 8,
+                Rank.Rank_9 => 9,
+                Rank.Rank_10 => 10,
+                Rank.Rank_Jack => 10,
+                Rank.Rank_Queen => 10,
+                Rank.Rank_King => 10,
+                Rank.Rank_Ace => 11,
+                _ => 0,
+            };
+        }
+    }
+}
---
+++ Deck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    internal class Deck
+    {
+        public int deckIndex;
+        public List<Card> deck;
+
+        public Deck() 
+        {
+            deck = new List<Card>();
+
+            foreach (Card.Rank rank in Enum.GetValues(typeof(Card.Rank)))
+            {
+                foreach (Card.Suit suit in Enum.GetValues(typeof(Card.Suit)))
+                {
+                    Card card = new(rank, suit);
+                    deck.Add(card);
+                }
+            }
+        }
+        public void PrintDeck()
+        {
+            foreach (Card card in deck)
+            {
+                Console.Write($"{card}, ");
+            }
+        }
+    }
+}
